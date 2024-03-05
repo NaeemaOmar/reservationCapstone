7c36f9868f99808a5deb4f01e950192dad795eff
NaeemaOmar <naeemaomar19@gmail.com>
2024-03-05 09:33:12 +0200

    checking if push works

--- reservation_frontend/src/views/AboutView.vue
+++ reservation_frontend/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is the about page after I changed it a bitjie</h1>
+    <h1>This is the about page after I changed it a bit more</h1>
   </div>
 </template>
