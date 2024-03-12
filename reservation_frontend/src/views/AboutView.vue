9c368d45198ef05df00c147f24b7451fbfe2c357
NaeemaOmar <naeemaomar19@gmail.com>
2024-03-12 16:40:46 +0200

    about page styling and content

--- reservation_frontend/src/views/AboutView.vue
+++ reservation_frontend/src/views/AboutView.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="about">
-    <h1>This is the about page after I changed it a bitjie</h1>
-    <h1>This is the about page after I changed it a bit more</h1>
+    <h1>About Home Affairs eBooking services</h1>
+    <h3 class="mainBrwnTxt">Easy to use, convenient and just a click away</h3>
+    <h5>
+      The Department of Home Affairs, as one of the vital departments in South
+      African government has always been inundated with a large number of
+      clients. <br /><br />
+      For registration of birth alone, with 1 003 307 births in 2020 and about
+      240 working days, the department would need to process on average, 4180
+      births a day. While this number is spread across the 184 offices
+      country-wide, if you add to this the number of marriages, divorces,
+      deaths, citizenship and other services Home Affairs offers, you can
+      understand just how many people frequent home affairs each day. This of
+      course results in long queues, some times starting as early as 5am.
+      <br /><br />
+      To alleviate the strain this has on our clients, Home Affairs began
+      offering online booking in 2022 allowing clients to register and
+      thereafter make an appointment at a specific time thereby by-passing the
+      need to arrive hours before you are able to meet with staff.
+      <br /><br />
+      It was our hope that this would create more pleasant opportunities for
+      both our clients as well as our staff and since then, this has been the
+      case.
+      <br /><br />
+      While the current booking system is not equally accessible to all South
+      Africans, we the Department of Home Affairs hope to extend the booking
+      options to cellphones so that the absolute majority of South Africans can
+      enjoy the benefits of the eBooking service.
+    </h5>
+    <h3 class="mainBrwnTxt">Home Affairs eBooking - Easy to use, convenient and just a click away</h3>
   </div>
 </template>
