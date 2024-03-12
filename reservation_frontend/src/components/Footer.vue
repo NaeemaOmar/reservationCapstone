7997b13d955313f590b6e3acf9f7997a8ed829ed
NaeemaOmar <naeemaomar19@gmail.com>
2024-03-12 09:30:18 +0200

    slight changes to footer

--- reservation_frontend/src/components/Footer.vue
+++ reservation_frontend/src/components/Footer.vue
@@ -1,20 +1,34 @@
 <template>
-    <div class="blackBg footerHeight">
-        <div>
+    <div class="blackBg footerHeight
+">
             <br>
-            <p class="mainBrwnTxt">Disclaimer: this is a dha eBooking clone made as part of a capstone project</p>
-            <p class="mainBrwnTxt">&copy NaeemaOmar 2024</p>
-        </div>
+            <div class="d-flex testBorder justify-content-between mx-3">
+                <p class="mainBrwnTxt">Disclaimer: this is a dha eBooking clone made as part of a capstone project</p>
+                <p class="mainBrwnTxt">& copy NaeemaOmar 2024</p>
+            </div>
     </div>
 </template>
 
 <style>
 .footerHeight{
-    height: 110px;
-    /* 130px height at 300px screen */
+    height: 100px;
+    /* 130px height at 700px screen */
+    /* 150px height at 700px screen */
+
 }
 
 .verticalCenter{
     margin-block: center;
 }
+
+.marginStart{
+    /* margin-inline-start: 0px; */
+    margin-inline: 0px;
+}
+
+.testBorder{
+    border: dotted;
+    border-color: aqua;
+}
+
 </style>
