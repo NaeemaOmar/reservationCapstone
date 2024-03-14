7f34e9e9b804bb2bbc5430e018f48fa4a3d9c086
NaeemaOmar <naeemaomar19@gmail.com>
2024-03-14 11:59:38 +0200

    loginStatus is sucessfully set to and called from localStorage

--- reservation_frontend/src/store/index.js
+++ reservation_frontend/src/store/index.js
@@ -37,23 +37,16 @@ export default createStore({
       try{
         let response = await axios.post(`${baseUrl}/users/login`, userInfo)
         console.log("below is the response of the axios.post (hopefully)")
-        commit('checkUserStatus',response.data)
+        console.log(response.data)
+        commit('checkUserStatus', response.data)
         console.log("The error is not at line 35")
         // let theCookie = $cookies.get("token")
         let theCookie = $cookies.isKey('token')
         console.log("The error is not at line 37. Below is the cookie (hopefully)")
         console.log(theCookie)
+        // The problem I'm having is that the above line is returning 'false' eventhough I can literally see the token in my cookies
         console.log("The error is not at line 39")
-
-        //The code below uses normal js instead of vue-cookies
-        // let token = response.headers['set-cookie'].find(cookie => cookie.startsWith('token')).split('=')[1];
-        // console.log(response);
-        // console.log("The error is not at line 38")
-        // localStorage.setItem('token', token)
-        // console.log("The error is not at line 4")
-        // console.log("below is the token '(hopefully)'")
-        // console.log(token)
-        // console.log("The error is not at line 40")
+        return response.data
       } catch (error){
         console.log(`in the axios, the following error was found while trying to check the user: ${error}`)
       }
--- reservation_frontend/src/views/LoginView.vue
+++ reservation_frontend/src/views/LoginView.vue
@@ -26,25 +26,40 @@
             <h5 class="d-flex justify-content-start mx-2">
               Username (ID number)
             </h5>
-            <input type="number" placeholder="Enter your ID number..." v-model="newUserInfo.userID"/>
+            <input
+              type="number"
+              placeholder="Enter your ID number..."
+              v-model="newUserInfo.userID"
+            />
             <h5 class="d-flex justify-content-start mx-2 mt-3">firstName</h5>
-            <input type="name" placeholder="Enter cellphone number..." v-model="newUserInfo.firstName"/>
+            <input
+              type="name"
+              placeholder="Enter cellphone number..."
+              v-model="newUserInfo.firstName"
+            />
             <h5 class="d-flex justify-content-start mx-2 mt-3">lastName</h5>
-            <input type="name" placeholder="Enter cellphone number..." v-model="newUserInfo.lastName"/>
+            <input
+              type="name"
+              placeholder="Enter cellphone number..."
+              v-model="newUserInfo.lastName"
+            />
             <h5 class="d-flex justify-content-start mx-2 mt-3">Password</h5>
-            <input type="password" placeholder="Enter password here..." v-model="newUserInfo.txtPassword"/>
+            <input
+              type="password"
+              placeholder="Enter password here..."
+              v-model="newUserInfo.txtPassword"
+            />
             <br /><br />
             <button class="my-3" @click="registerNewUser()">Register</button>
           </div>
         </div>
         <div class="col-12 col-sm-5 mx-1 mainBrwnBg">
-            <div class="mainWhitishBg innerDiv">
+          <div class="mainWhitishBg innerDiv">
             <h5 class="text-start mx-2">
               To login, enter your username (ID number) and password.
-              <br>
-              <span class="mainBrwnTxt"
-                >Register</span
-              > if you do not have an account.
+              <br />
+              <span class="mainBrwnTxt">Register</span> if you do not have an
+              account.
               <br />
               A <span class="mainBrwnTxt">one-time-pin (OTP)</span> will be sent
               to your cellphone so please ensure you have your cellphone near
@@ -57,12 +72,26 @@
             <h5 class="d-flex justify-content-start mx-2">
               Username (ID number)
             </h5>
-            <input type="number" class="d-flex justify-content-start mx-2" placeholder="Enter your ID number..." v-model="checkUserInfo.userID"/>
+            <input
+              type="number"
+              class="d-flex justify-content-start mx-2"
+              placeholder="Enter your ID number..."
+              v-model="checkUserInfo.userID"
+            />
             <h5 class="d-flex justify-content-start mx-2 mt-3">Password</h5>
-            <input type="password" class="d-flex justify-content-start mx-2" placeholder="Enter password here..." v-model="checkUserInfo.txtPassword" />
+            <input
+              type="password"
+              class="d-flex justify-content-start mx-2"
+              placeholder="Enter password here..."
+              v-model="checkUserInfo.txtPassword"
+            />
             <h5 class="d-flex justify-content-start mx-2 mt-3">OTP</h5>
-            <input class="d-flex justify-content-start mx-2" type="number" placeholder="Enter cellphone number..." />
-            <br>
+            <input
+              class="d-flex justify-content-start mx-2"
+              type="number"
+              placeholder="Enter cellphone number..."
+            />
+            <br />
             <button class="my-3" @click="checkUser()">Login</button>
           </div>
         </div>
@@ -78,8 +107,8 @@
       </thead>
       <tbody>
         <tr v-for="user in this.$store.state.allTheUsers" :key="user.userID">
-        <td>{{ user.userID }}</td>
-        <td>{{ user.token }}</td>
+          <td>{{ user.userID }}</td>
+          <td>{{ user.token }}</td>
         </tr>
       </tbody>
     </table>
@@ -96,11 +125,11 @@
       </thead>
       <tbody>
         <tr v-for="user in this.$store.state.allTheUsers" :key="user.userID">
-        <td>{{ user.userID }}</td>
-        <td>{{ user.firstName }}</td>
-        <td>{{ user.lastName }}</td>
-        <td>{{ user.txtPassword }}</td>
-        <td>{{ user.hashedPassword }}</td>
+          <td>{{ user.userID }}</td>
+          <td>{{ user.firstName }}</td>
+          <td>{{ user.lastName }}</td>
+          <td>{{ user.txtPassword }}</td>
+          <td>{{ user.hashedPassword }}</td>
         </tr>
       </tbody>
     </table>
@@ -109,47 +138,88 @@
 
 <script>
 export default {
-  data (){
-    return{
-      newUserInfo:{
-        userID:null,
-        firstName:null,
-        lastName:null,
-        txtPassword: null
+  data() {
+    return {
+      newUserInfo: {
+        userID: null,
+        firstName: null,
+        lastName: null,
+        txtPassword: null,
       },
-      checkUserInfo:{
-        userID:null,
-        txtPassword: null
+      checkUserInfo: {
+        userID: null,
+        txtPassword: null,
       },
-      loginStatus: null
-  }},
-  methods:{
-    async registerNewUser(){
-      try{
-        console.log("the register user method in the login page is running")
-        await this.$store.dispatch('registerUser', this.newUserInfo)
-      } catch(error){
-        console.log(`the following error was found while trying to register a new user in the login pg: ${error}`)
+      loginStatus: null,
+    };
+  },
+  methods: {
+    async registerNewUser() {
+      try {
+        console.log("the register user method in the login page is running");
+        await this.$store.dispatch("registerUser", this.newUserInfo);
+      } catch (error) {
+        console.log(
+          `the following error was found while trying to register a new user in the login pg: ${error}`
+        );
       }
     },
-    async checkUser(){
-      try{
-        console.log("the checkUser method in the login page is running")
-        await this.$store.dispatch('checkAUser', this.checkUserInfo)
-        this.loginStatus = this.$store.state.loginStatus 
-      }catch(error){
-        console.log(`the following error was found while trying to check user credentials in the login pg: ${error}`)
+    // below (line 136 till 146) is the perfectly functioning checkUser fx. I want to find a way to store the true/false in local storage but I don't want to lose my original so I'm commenting it out
+    // async checkUser(){
+    //   try{
+    //     console.log("the checkUser method in the login page is running")
+    //     let checkTheuser = await this.$store.dispatch('checkAUser', this.checkUserInfo)
+    //     console.log("We're back in the checkUser fx of the loginPg. Below is the value of the checkTheUser variable (hopefully)")
+    //     console.log(checkTheuser)
+    //     this.loginStatus = checkTheuser
+    //   }catch(error){
+    //     console.log(`the following error was found while trying to check user credentials in the login pg: ${error}`)
+    //   }
+    // },
+    async checkUser() {
+      try {
+        console.log("the checkUser method in the login page is running");
+        let checkTheuser = await this.$store.dispatch(
+          "checkAUser",
+          this.checkUserInfo
+        );
+        console.log(
+          "We're back in the checkUser fx of the loginPg. Below is the value of the checkTheUser variable (hopefully)"
+        );
+        console.log(checkTheuser);
+        console.log(
+          "below i'm setting the checkTheuser variable to local storage. hopefully it works"
+        );
+        localStorage.setItem("checkTheuser", JSON.stringify(checkTheuser));
+        location.reload()
+      } catch (error) {
+        console.log(
+          `the following error was found while trying to check user credentials in the login pg: ${error}`
+        );
       }
+    },
+  },
+  mounted() {
+    try {
+      this.$store.dispatch("getTheUsers");
+    } catch (error) {
+      console.log(
+        `the following error was found while trying to mount the user in the login pg: ${error}`
+      );
+    }
+    // trying to set the loginSatus fr localStorage
+    try {
+      let frLocalStorage = JSON.parse(localStorage.getItem("checkTheuser"));
+      console.log("below is whatever is returned fr the local storage in the mounted of the loginPg");
+      console.log(frLocalStorage);
+      this.loginStatus = frLocalStorage;
+    } catch (error) {
+      console.log(
+        `The following error was found while trying to set the oginStatus at the mounted of the loginPg: ${error}`
+      );
     }
   },
-  mounted () {
-    try{
-      this.$store.dispatch('getTheUsers')
-    }catch(error){
-      console.log(`the following error was found while trying to mount the user in the login pg: ${error}`)
-    }
-  }
-}
+};
 </script>
 
 <style>
@@ -163,9 +233,8 @@ h3 {
   margin-block: 12px;
 }
 
-tr{
+tr {
   border: dotted;
   border-color: blue;
 }
-
 </style>
