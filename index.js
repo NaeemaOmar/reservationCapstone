b71145c316e3f282906972b8ccfe382745ec664a
NaeemaOmar <naeemaomar19@gmail.com>
2024-03-15 15:45:49 +0200

    changed corsOptions origin

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express()
 
 const corsOptions = {
     // Put in the firebase address here so Godwin can access it
-    origin: "http://localhost:8080",
+    origin: "http://localhost:8081",
     credentials:true,
     // Please work, amen
     optionsSuccessStatus:200
