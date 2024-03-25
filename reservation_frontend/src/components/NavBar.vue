7d592e70f6505de33036c3dea4261db550761b24
NaeemaOmar <naeemaomar19@gmail.com>
2024-03-25 10:38:17 +0200

    properly delete redundant view

--- reservation_frontend/src/components/NavBar.vue
+++ reservation_frontend/src/components/NavBar.vue
@@ -42,9 +42,6 @@
               <a class="nav-link brownColor" href="/admin" v-if="adminUser">Admin</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link brownColor" href="/datefx">Bookings</a>
-            </li>
-            <li class="nav-item">
               <a class="nav-link" href="/">
                 <button @click="logOut()">Log out</button>
               </a>
